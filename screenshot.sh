43c5632f18ccac55be20e4018bdfb0f77dfd7233
ToanTran <toan.tran@eastagile.com>
2016-01-20 16:18:46 +0700

    [r] Better pattern for naming screenshot

--- screenshot.sh
+++ screenshot.sh
@@ -1,4 +1,7 @@
 SCREENSHOT_NAME=$1
+NOW=$(date +"%m-%d-%Y-%H-%M-%S")
+SPLITTER="_"
+NAME=$SCREENSHOT_NAME$SPLITTER$NOW
 echo Taking screenshot with name $SCREENSHOT_NAME
 
-adb shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g' > $ANDROID_SCREENSHOT/$SCREENSHOT_NAME.png
+adb shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g' > $ANDROID_SCREENSHOT/$NAME.png
