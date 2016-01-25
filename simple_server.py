056dd6834105943279b1bbf4515139460bda4d80
ToanTran <toan.tran@eastagile.com>
2016-01-25 10:21:24 +0700

    [f] Support unlock operation.

--- simple_server.py
+++ simple_server.py
@@ -3,24 +3,42 @@ from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 from subprocess import call
 
 PORT = 9000
+ACTION_UNLOCK = "unlock"
+ACTION_SCREENSHOT = "screenshot"
+PARAM_FILENAME = "filename"
 
 class ScreenshotHandler(BaseHTTPRequestHandler):
 	def do_POST(self):
-		file_name = self.headers.getheader('filename', 'screenshot')
-		self.screenshot(file_name)
-		self.send_response(200)
-  		self.send_header("Content-type", "application/json ")
-  		self.end_headers()
-		self.wfile.write(self.dump_screenshot_response(file_name))
+		action = self.headers.getheader('action', 'unlock') # default action is unlock device screen
+		if action == ACTION_SCREENSHOT:
+			file_name = self.headers.getheader(PARAM_FILENAME, 'screenshot')
+			self.screenshot(file_name)
+			self.dump_screenshot_response(file_name)
+		elif action == ACTION_UNLOCK:
+			self.unlock()
+			self.dump_unlock_response()
+
 		
 	def do_GET(self):
 		print "GET operation not supported!"
 
 	def dump_screenshot_response(self, file_name):
-		return {"file":file_name}
+		self.send_response(200)
+  		self.send_header("Content-type", "application/json ")
+  		self.end_headers()
+  		self.wfile.write({"file":file_name})
+
+	def dump_unlock_response(self):
+		self.send_response(200)
+  		self.send_header("Content-type", "application/json ")
+  		self.end_headers()
+		self.wfile.write("Unlock screen ok")
 
 	def screenshot(self, name):
 		call(["./screenshot.sh "+ name], shell=True)
+
+	def unlock(self):
+		call(["./unlock.sh"], shell=True)
 
 server = HTTPServer(('', PORT), ScreenshotHandler)
 print "Android CI Bridge serving at port: ", PORT
---
+++ unlock.sh
@@ -0,0 +1,6 @@
+echo "Unlock screen"
+# Press MENU to unlock
+adb shell input keyevent 82
+
+# Press HOME to close any other screens showing
+adb shell input keyevent 3
