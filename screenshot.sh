147726126d4e0e24483d827a39a8ac6bcad2e305
ToanTran <toan.tran@eastagile.com>
2016-01-28 16:58:22 +0700

    [r] Should proceed screenshot as fast as possible.

--- screenshot.sh
+++ screenshot.sh
@@ -3,11 +3,13 @@ TRACE=$2
 NOW=$(date +"%m-%d-%Y-%H-%M-%S")
 SPLITTER="_"
 NAME=$TAG$SPLITTER$NOW
-echo Taking screenshot with name $TAG
+
+adb shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g' > $ANDROID_SCREENSHOT/$NAME.png
+
 mkdir -p $ANDROID_SCREENSHOT/$TAG
 
 if [[ ! -z $TRACE ]]
 	then echo $TRACE >> $ANDROID_SCREENSHOT/$TAG/trace$SPLITTER$NOW.txt
 fi
 
-adb shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g' > $ANDROID_SCREENSHOT/$TAG/$NAME.png
+mv $ANDROID_SCREENSHOT/$NAME.png $ANDROID_SCREENSHOT/$TAG
